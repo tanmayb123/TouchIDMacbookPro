b1b9dc3a09827a455075dab28f10f3f0849265e6
Tanmay Bakshi <tanmaybakshi@Tanmays-MacBook-Pro.local>
2016-11-18 17:33:02 -0500

    Initial Commit

---
+++ TouchIDVaultMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchIDVaultMac
+//
+//  Created by Tanmay Bakshi on 2016-11-18.
+//  Copyright © 2016 Tanmay Bakshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchIDVaultMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchIDVaultMac
+//
+//  Created by Tanmay Bakshi on 2016-11-18.
+//  Copyright © 2016 Tanmay Bakshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
